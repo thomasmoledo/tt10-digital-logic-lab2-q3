a1147b810404758a230476dc866715f75806d3fd
Thomas Moledo <77012252+thomasmoledo@users.noreply.github.com>
2025-02-27 20:47:19 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_q1 user_project (
+  tt_um_bitwise_and user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
